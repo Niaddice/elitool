0756f5379d781956b1287b88fb36d10dd6c45b87
佳斌 倪 <nijb@shresource.com.cn>
2021-08-11 11:35:47 +0800

    添加git ignore

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"elitool/structs"
+	"fmt"
+)
 
+type stru struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	st := stru{Name: "姓名", Age: 1}
+	m := structs.Struct2map(st, "Name", "")
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
--- structs/StructUtils.go
+++ structs/StructUtils.go
@@ -1,9 +1,32 @@
 package structs
 
-import "reflect"
+import (
+	"reflect"
+)
 
-func struct2map(resource interface{}) map[string]interface{} {
-	typeOf := reflect.TypeOf(resource)
-	typeOf.Name()
-	return nil
+// 结构体相关工具类
+
+// Struct2map
+// @param resourceStruct 需要转换的结构体
+// @param keyField 主键名称,结构体字段名称
+// @param valueField 指定值的字段名称,当value是"" 空字符时,返回的map为 map[key]resource
+func Struct2map(resourceStruct interface{}, keyField string, valueField string) map[string]interface{} {
+	result := make(map[string]interface{})
+	if resourceStruct == nil {
+		panic("结构体不能为nil")
+	}
+	if valueField == "" {
+		t := reflect.ValueOf(resourceStruct)
+		if t.Kind() == reflect.Struct {
+			keyValue := reflect.ValueOf(resourceStruct).FieldByName(keyField).String()
+			result[keyValue] = nil
+			if valueField != "" {
+				value := reflect.ValueOf(resourceStruct).FieldByName(valueField).String()
+				result[keyValue] = value
+			} else {
+				result[keyValue] = resourceStruct
+			}
+		}
+	}
+	return result
 }
